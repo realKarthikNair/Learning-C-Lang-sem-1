3fcd5e53bf68045adb83046e74f33fb1a0a360ab
Karthik Nair <knairothera@gmail.com>
2022-01-14 14:43:07 +0530

    Add files via upload

--- basics/programs/leap year.c
+++ basics/programs/leap year.c
@@ -7,25 +7,14 @@ int main()
         int year;
         printf("Enter an year: ");
         scanf("%d",&year);
-        printf("Year is ");
-        if (year%4==0)
+        printf("%d is ", year);
+        if ((year%4==0)&&((year%400==0)||((year%100)!=0)))
         {
-                if (year%400==0)
-                {
-                        printf("a leap year");
-                }
-                else if ((year%100)!=0)
-                {
-                        printf("a leap year");
-                }
-                else
-                {
-                        printf("not a leap year");
-                }
+        	printf("a leap year");
         }
         else
         {
-                printf("not a leap year");
+        	printf("not a leap year");
         }
         printf("\n");
 }
