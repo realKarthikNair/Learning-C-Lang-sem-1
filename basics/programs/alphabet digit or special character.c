44d96e63043e6132973acace3074527e8631045b
Karthik Nair <knairothera@gmail.com>
2022-01-17 16:06:01 +0530

    added qn 9

---
+++ basics/programs/alphabet digit or special character.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	char a;
+	printf("Enter a character: ");
+	scanf("%c", &a);
+	printf("Entered character is ");
+	if (((a>='a')&&(a<='z'))||((a>='A')&&(a<='Z')))
+	{
+		printf("an alphabet");
+	}
+	else if ((a>='0')&&(a<='9'))
+	{
+		printf("a number");
+	}
+	else
+	{
+		printf("a special character");
+	}
+	printf("\n");
+}	
