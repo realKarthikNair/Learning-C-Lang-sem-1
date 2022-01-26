f6e3b3c2252fe6dd53e514c09b5a1d10b3ff017b
realkarthiknair <knairothera@gmail.com>
2022-01-27 02:23:48 +0530

    haha

---
+++ basics/programs/pattern7.c
@@ -0,0 +1,18 @@
+/*50. Display the following pattern on screen( inverted full pyramid): ( Up to n n)
+
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+     * * *
+       *
+
+*/  
+
+// THIS CODE IS INCOMPLETE
+
+#include <stdio.h>
+
+int main()
+{
+    int i=1, 
+}
