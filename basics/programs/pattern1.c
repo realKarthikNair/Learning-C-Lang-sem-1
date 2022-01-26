56cfb5a6831975403e7108e189200b23f1ebeaa7
realkarthiknair <knairothera@gmail.com>
2022-01-26 23:05:28 +0530

    haha

---
+++ basics/programs/pattern1.c
@@ -0,0 +1,22 @@
+/*Display the following pattern on screen: (Up to n rows)
+
+*
+**
+****/
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, j;
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+    for (i=1; i<=n; i++)
+    {
+        for (j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
