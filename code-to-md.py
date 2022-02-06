747eeed8ab492e6d7510b3cd7c100a0a900938c3
realkarthiknair <knairothera@gmail.com>
2022-02-07 00:36:11 +0530

    update md_generator

--- code-to-md.py
+++ code-to-md.py
@@ -12,11 +12,11 @@ for i in sorted(os.listdir("programs")):
         while True:
             title=f"### {data[j]}"; line_1=f"### {sno}. {data[j]}"
             if data[0][0:2]=='//' and flag1==1:
-                md_file.write(line_1);j+=1; sno+=1
+                md_file.write(line_1); sno+=1
                 flag1=0
             elif data[0][0:2]=="/*" and flag1==1:
                 md_file.write(line_1)
-                j+=1
+            
                 flag=True
                 flag1=0
                 sno+=1
@@ -24,26 +24,22 @@ for i in sorted(os.listdir("programs")):
                 md_file.write(f"### {sno}.\n")
                 sno+=1
                 flag1=0
-                j+=1
+            
             elif flag==True:
                 if data[j][-3:-1]=="*/":
                     md_file.write(title)
-                    j+=1
+                
                     flag=False
                 else:
                     md_file.write(title)
-                    j+=1
+                
             else:
                 md_file.write(f"\t{data[j]}")
-                j+=1
+            j+=1
+            
     except:
         md_file.write("\n\n")
         continue
     print(f"{output_filename} saved successfully at {output_path} !")
     
-
-
-
-
-# done
 
