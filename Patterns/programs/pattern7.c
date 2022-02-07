33ee21c95a2c20363c61579f5b6f00e7f19d97df
realkarthiknair <knairothera@gmail.com>
2022-02-08 02:14:59 +0530

    documented pattern 7.c

--- Patterns/programs/pattern7.c
+++ Patterns/programs/pattern7.c
@@ -8,6 +8,11 @@
 
 */
 
+// logic
+// The number of stars is ((2*i)-1) for each row (eg in the 5th row, it is (2*5)-1=9 as in the question )
+// The number total of spaces in a row is the same as i
+
+
 #include <stdio.h>  
 int main()  
 {   
