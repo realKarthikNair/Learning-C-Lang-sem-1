9956819128fd0e87f871d5eb797a6ba667266f0d
realkarthiknair <knairothera@gmail.com>
2022-02-17 02:26:41 +0530

    add

--- Learning_C/5. Loops/1. For loops/programs1 (patterns)/programs/pattern8.c
+++ Learning_C/5. Loops/1. For loops/programs1 (patterns)/programs/pattern8.c
@@ -16,3 +16,17 @@
 
 */
 
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter value of n (number of columns): ");
+    scanf("%d", &n);
+    for (i=1; i<=n; i++)
+    {
+        if(i<((n*2)-1))
+        {
+            for (j=1; )
+        }
+    }
+}
