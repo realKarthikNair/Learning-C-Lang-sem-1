f7303176b652ab31f30afb9acd1adecd6a84e050
realkarthiknair <knairothera@gmail.com>
2022-02-28 02:00:12 +0530

    add more code

---
+++ Learning_C/12. Dynamic memory allocation/programs.md
@@ -0,0 +1,62 @@
+### 1. // WAP to dynamically create an array of given user and display it in ascendiong order
+```c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int *);
+
+int main()
+{
+    // Declaration, memory allocation and checking for successful allocation
+    int len, i, j, temp;
+    printf("Enter length of array: ");
+    scanf("%d", &len);
+    int* array= calloc(len, sizeof(int));
+    if (array == NULL)
+    {
+        printf("Out of memory\n");
+    }
+
+    // initializion of array
+    for (i=0; i<len; i++)
+    {
+        printf("input value %d: ", i+1);
+        scanf("%d", &array[i]);
+    }   
+
+    printf("Entered array is\n");
+    for (i=0; i<len; i++)
+    {
+        printf("%d, ", *(array+i));
+    }
+
+    printf("\n");
+
+    // sorting of array
+
+    for (i=0; i<len; i++)
+    {
+        for (j=i+1; j<len; j++)
+        {
+            if (*(array+i) > *(array+j))
+            {
+                temp = *(array+i);
+                *(array+i)=*(array+j);
+                *(array+j)=temp;
+            }
+        }
+    }
+
+    // printing sorted array
+
+    printf("Array after sorting is\n");
+    for (i=0; i<len; i++)
+    {
+        printf("%d, ", *(array+i));
+    }
+    
+    free(array);
+}
+```
+
--- Learning_C/8. Pointers/programs/3. palindrome_with_pointers.c
+++ Learning_C/8. Pointers/programs/3. palindrome_with_pointers.c
@@ -1,65 +1,4 @@
-### 1. // Program to demonstrate pointers in C
-```c
-
-#include <stdio.h>
-int main()
-{
-    int num = 1;  //declared an integer variable named num with value 10
-    int *p;  //declared variable p of type pointer
-    p = &num; //p is pointing to the address of the variable num of type integer.
-    printf("value of p is %p\n", p);
-    printf("Value of &p is is %p\n", &p);
-    printf("value of *p is %d\n", *p);
-    printf("value of num is %d\n", num);
-    printf("Value of &num is %p\n", &num);
-}
-
-```
-
-### 2. // Swap two variables using pointers
-```c
-
-#include<stdio.h> 
-
-void swap (int* x,int* y);
-
-int main()
-
-{
-
-    int a,b;
-
-    printf("Enter Value of a: ");
-
-    scanf("%d",&a); 
-
-    printf ("Enter Value of b: ");
-
-    scanf("%d", &b);
-
-    printf("Before swapping\n Value of a is %d \n Value of b is %d \n", a, b);
-
-    swap (&a, &b);
-
-    printf("After swapping\n Value of a is %d \n Value of b is %d", a, b);
-
-}
-
-void swap (int* x ,int* y)
-
-{
-
-    int t=*x;
-
-    *x=*y;
-
-    *y=t;
-
-}
-```
-
-### 3. // WAP to check if a number is palindrome or not using functions and pointers
-```c
+// WAP to check if a number is palindrome or not using functions and pointers
 
 
 #include <stdio.h>
@@ -115,5 +54,3 @@ int main()
         printf("Number is not a Palindrome!\n");
     }
 }
-```
-
