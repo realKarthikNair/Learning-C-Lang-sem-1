6666a9d0fc07b461ac6b4b196a9912cf9511729a
realkarthiknair <knairothera@gmail.com>
2022-03-05 00:48:39 +0530

    structures

--- Learning_C/12. Strings/programs/2. check for palindrome.c
+++ Learning_C/12. Strings/programs/2. check for palindrome.c
@@ -1,20 +1,9 @@
 // // Write a program to check if a string is palindrome or not
 
 // #include <stdio.h>
-// int reverse(char * word[(12*sizeof(char))]);
-
-// int reverse(char * word[(12*sizeof(char))])
-// {
-//     int len=sizeof(*(word));
-//     printf("%d", len);
-// }
-
+// #include <string.h>
 
 // int main()
 // {
-//     // Initializing a string that is palindrome
-//     char string1[]="aibohphobia";
-//     // initializing a string that is not a palindrome
-//     char string2[]="mathematics";
-//     reverse(char* string1[12]);
+
 // }
---
+++ Learning_C/14. Structures/programs/structures_practice_problem2.c
@@ -0,0 +1,63 @@
+/*WAP to create a structure of employee with id, name, dept and age.
+Also implement a linear search and display details of user with id as the search parameter
+*/
+
+#include <stdio.h>
+
+int main()
+{
+   int i=0;
+   typedef struct employee
+   {
+      long int id;
+      char name[10];
+      char dept[30];
+      int age ;
+   }emp;
+   
+   emp list[5];
+   
+   for (i = 0; i<5;i++)
+   {
+      printf("Enter id of employee %d : ", i+1);
+      scanf("%ld", &(list[i].id));
+      printf("Enter name of employee %d : ", i+1);
+      scanf("%s",list[i].name);
+      printf("Enter dept of employee %d : ", i+1);
+      scanf("%s", list[i].dept);
+      printf("Enter age of employee %d : ", i+1);
+      scanf("%d", &(list[i].age));
+   }
+   // printf("Data Enterd is as follows:\n");
+   // for (int i = 0; i<5;i++)
+   // {
+   //    printf("Id : %ld \n", list[i].id);
+   //    printf("Name : %s \n", list[i].name);
+   //    printf("Dept : %s \n", list[i].dept);
+   //    printf("Age : %d\n", list[i].age);
+   // }
+
+   long int emp_id;
+   int flag=0;
+   printf("Enter employee id to be searched: ");
+   scanf("%ld", &emp_id);
+
+   for (i=0; i<5; i++)
+   {
+      if (list[i].id==emp_id)
+      {
+         printf("Employee found!!");
+         flag=1;
+         printf("Id : %ld \n", list[i].id);
+         printf("Name : %s \n", list[i].name);
+         printf("Dept : %s \n", list[i].dept);
+         printf("Age : %d\n", list[i].age);
+      }
+   }
+   if (flag==0)
+   {
+      printf("Employee not found!!");
+   }
+}
+
+
