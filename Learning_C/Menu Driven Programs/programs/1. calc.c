3e2dbea5bf6850db1d8e8a368afd761cfe5eae2c
realkarthiknair <knairothera@gmail.com>
2022-03-14 01:33:35 +0530

    small changes

--- Learning_C/Menu Driven Programs/programs/1. calc.c
+++ Learning_C/Menu Driven Programs/programs/1. calc.c
@@ -4,56 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float return_sum(float*,int len);
-float return_sub(float*,int len);
-float return_prod(float*,int len);
-
-float percentage(float ,float);
-
-float return_sum(float* nums,int len)
-{
-    float sum=0;
-    for (int i=0; i<len; i++)
-    {
-        float num= (nums[i]);
-        sum += num;
-    }
-    return sum;
-}
-
-float return_sub(float* nums,int len)
-{
-    float init=nums[0];
-    float sub=init;
-    for (int i=1; i<len; i++)
-    {
-        float num= (nums[i]);
-        sub -= num;
-    }
-    return sub;
-}
-
-float return_prod(float* nums,int len)
-{
-    float prod=1;
-    for (int i=0; i<len; i++)
-    {
-        float num= (nums[i]);
-        prod*=num;
-    }
-    return prod;
-}
-
-float percentage(float p, float n)
-{
-    float percent=(n/100)*p;
-    return percent;
-}
-
 int main()
 {
-    int operation, len;
-    float* nums, sum;
+    int i, operation, len;
+    float sum, num, sub, prod, percent;
     while (1)
     {
         printf("\nWhich operation do you want to carry out?\nEnter 1 for addition\n 2 for subtraction\n 3 for multiplication\n 4 for division\n 5 for average\n 6 for percentage \n7 to exit\nEnter here: ");
@@ -61,48 +15,42 @@ int main()
         switch (operation)
         {
         case 1:
-           
+            sum=0;
             printf("How many Numbers to add?\nEnter Here :");
             scanf("%d", &len);
-            nums= calloc(len, sizeof(float));
-            sum=0;
-            for (int i=0; i<len; i++)
+            for (i=0; i<len; i++)
             {
-                float value;
-                printf("Enter Number %d: ", i+1);
-                scanf("%f", &value);
-                nums[i] = value;
+                printf("Enter number %d: ", i+1);
+                scanf("%f", &num);
+                sum+=num;
             }
-            sum= return_sum(nums,len);
             printf("sum is %f", sum);
             break;
         case 2:
-
             printf("How many Numbers to subtract?\nEnter Here :");
             scanf("%d", &len);
-            nums= calloc(len, sizeof(float));
-            for (int i=0; i<len; i++)
+            int first;
+            printf("Enter number 1: ");
+            scanf("%d", &first);
+            sub=first;
+            for (i=1; i<len; i++)
             {
-                float value;
-                printf("Enter Number %d: ", i+1);
-                scanf("%f", &value);
-                nums[i] = value;
+                printf("Enter number %d: ", i+1);
+                scanf("%f", &num);
+                sub-=num;
             }
-            float sub= return_sub(nums,len);
             printf("Result after subtracting is %f.", sub);
             break;
         case 3:
             printf("How many Numbers to take product of?\nEnter Here :");
             scanf("%d", &len);
-            nums= calloc(len, sizeof(float));
-            for (int i=0; i<len; i++)
+            prod=1;
+            for (i=0; i<len; i++)
             {
-                float value;
-                printf("Enter Number %d: ", i+1);
-                scanf("%f", &value);
-                nums[i] = value;
+                printf("Enter number %d: ", i+1);
+                scanf("%f", &num);
+                prod*=num;
             }
-            float prod= return_prod(nums,len);
             printf("Product is %f.", prod);
             break;
         case 4:
@@ -116,16 +64,13 @@ int main()
         case 5:
             printf("How many Numbers to average?\nEnter Here :");
             scanf("%d", &len);
-            nums= calloc(len, sizeof(float));
             sum=0;
-            for (int i=0; i<len; i++)
+            for (i=0; i<len; i++)
             {
-                float value;
-                printf("Enter Number %d: ", i+1);
-                scanf("%f", &value);
-                nums[i] = value;
+                printf("Enter number %d: ", i+1);
+                scanf("%f", &num);
+                sum+=num;
             }
-            sum= return_sum(nums,len);
             printf("Average is %f", sum/len);
             break;
         case 6:
@@ -135,7 +80,8 @@ int main()
             scanf("%f", &n);
             printf("Enter what percentage of it is to be calculated: ");
             scanf("%f", &p);
-            printf("%f percentage of %f is %f", p, n, percentage(p, n));
+            percent=(n/100)*p;
+            printf("%f percentage of %f is %f", p, n, percent);
             break;
         case 7:
             printf("Abort!\n");return 0;
