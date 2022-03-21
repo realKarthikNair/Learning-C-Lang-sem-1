9b73fd07fa140bf9ddb8c1b83b9137266d06112a
realkarthiknair <knairothera@gmail.com>
2022-03-21 23:16:03 +0530

    small changes

--- Learning_C/Misc/code-to-md.py
+++ Learning_C/Misc/code-to-md.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 
-md_file=open(f"readme.md","w"); sno=1
+md_file=open(f"programs.md","w"); sno=1
 for i in sorted(os.listdir("programs")):
     path=os.path.join("programs", i)
     file=open(path, 'r')
@@ -10,7 +10,7 @@ for i in sorted(os.listdir("programs")):
         flag=False; first=True
         rt=1
         while True:
-            title=f"### {data[j]}"; line_1=f"### {sno}. {data[j]}"
+            title=f"### {data[j]}"; line_1=f"### {sno}. {data[j][2:]}"
             if data[0][0:2]=='//' and j==0:
                 md_file.write(line_1)
                 sno+=1
@@ -24,7 +24,9 @@ for i in sorted(os.listdir("programs")):
             elif flag==True:
                 if data[j][-3:-1]=="*/":
                     flag=False
-                md_file.write(title)
+                    md_file.write(title)
+                else:
+                    md_file.write(title)
             else:
                 if (first!=True) and (j!=(len(data)-1)): md_file.write(f"{data[j]}")
                 elif first==True:
@@ -38,3 +40,5 @@ for i in sorted(os.listdir("programs")):
         md_file.write("\n\n")
         continue
     print(f"{output_filename} saved successfully at {output_path} !")
+    
+
