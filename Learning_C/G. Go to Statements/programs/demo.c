86b308bd580589e2d0a9e3dd2ee989356cd29dd2
AroraKaran19 <karora0319@gmail.com>
2022-03-23 17:45:31 +0530

    demo.c

--- Learning_C/G. Go to Statements/programs/demo.c
+++ Learning_C/G. Go to Statements/programs/demo.c
@@ -7,12 +7,13 @@
 #include <stdio.h>
 
 int main(){
-    int number;
+    int number, i=0;
 
-    for (int i=0; i<=10; i++){
-        goto number;
-    }
-
-    number:
+    printnumber:
     printf("%d", number);
+
+    number++;
+    if (number<=10){
+    goto printnumber;
+    }
 }
