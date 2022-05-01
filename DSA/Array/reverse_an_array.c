1d2a9a3f0db4790026806656f96c1a1d768df7fd
realkarthiknair <knairothera@gmail.com>
2022-05-02 00:54:23 +0530

    reverse an array

---
+++ DSA/Array/reverse_an_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n=6, array0[]={1,22,33,65,22,54};
+    for (i=0; i<(n/2); i++)
+    {
+        int temp=array0[i];
+        array0[i]=array0[(n-1)-i];
+        array0[(n-1)-i]=temp;
+    }
+
+    printf("Array after reversing is: \n");
+    for (i=0; i<n; i++)
+    {
+        printf("%d, ", array0[i]);
+    }
+    printf("\n");
+}
